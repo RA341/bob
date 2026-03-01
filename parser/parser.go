4f7900a88c6acfa87042aadcd10fe1e276605a78
RA341 <razerishi1505@gmail.com>
2026-03-01 13:29:00 -0500

    basic parse test

--- parser/parser.go
+++ parser/parser.go
@@ -1,15 +1,45 @@
 package parser
 
+import "fmt"
+
 type Parser struct {
 	tokens []Token
 
+	start   int
 	current int
 }
 
 func NewParser(token []Token) {
+	t := Parser{
+		tokens: token,
+	}
 
+	t.Parse()
 }
 
-func (t *Token) Parse() {
+func (t *Parser) Parse() {
+	for !t.isAtEnd() {
+		t.start = t.current
+		t.ParseToken()
+	}
+}
 
+func (t *Parser) ParseToken() {
+	tok := t.Next()
+
+	fmt.Println(tok.String())
+}
+
+func (t *Parser) Peek() Token {
+	return t.tokens[t.current]
+}
+
+func (t *Parser) Next() Token {
+	s := t.Peek()
+	t.current += 1
+	return s
+}
+
+func (t *Parser) isAtEnd() bool {
+	return t.Peek().TokenType == EOF
 }
--- parser/parser_test.go
+++ parser/parser_test.go
@@ -1,1 +1,23 @@
 package parser
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/require"
+)
+
+func TestParser(t *testing.T) {
+	content := `
+cond1 = value
+cond2 = value2
+
+if cond1 == cond2 {
+	print("cond1 is equal")
+}
+`
+
+	lex := NewLexer([]byte(content))
+	require.Nil(t, lex.errs)
+
+	NewParser(lex.tokens)
+}
